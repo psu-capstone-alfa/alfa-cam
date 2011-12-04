609806725f0b85529170b5420f5533aba1ff80db
Charles <seadaily@hydra.(none)>
2011-12-03 22:16:52 -0800

    Added term definition

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111202070334) do
+ActiveRecord::Schema.define(:version => 20111204061347) do
 
   create_table "courses", :force => true do |t|
     t.string   "dept_code"
     t.string   "course_num"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "terms", :force => true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
