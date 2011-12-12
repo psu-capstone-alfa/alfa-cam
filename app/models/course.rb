3636ebf53f65f0bc5cc7332c4f5d1cd1e1712608
Charles <seadaily@hydra.(none)>
2011-12-11 22:37:31 -0800

    term/course edit/delete logic

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,14 @@
 class Course < ActiveRecord::Base
   has_many :offerings
+
+  before_destroy :check_for_offerings
+  before_update :check_for_offerings
+
+  def check_for_offerings()
+    if self.offerings.count > 0
+      self.errors[:base] << "Sorry, you cannot edit or delete a course with offerings."
+      false
+    end
+  end
+
 end
