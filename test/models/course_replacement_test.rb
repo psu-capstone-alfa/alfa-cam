fcf7aa75db68574b9f80f8fd42b7f39d6de7d714
Ryan Souza <ryan@ryansouza.net>
2011-12-30 18:35:06 -0800

    Validate Course attributes
    Test that attributes are required
    Fix course replacement test to provide required attributes

--- test/models/course_replacement_test.rb
+++ test/models/course_replacement_test.rb
@@ -2,10 +2,14 @@ require "minitest_helper"
 
 #class CourseReplacementTest < MiniTest::Rails::Model
 describe CourseReplacement do
+  before do
+    @valid_course_attrs = { dept_code: 'C', course_num: '123', title: 'Testing' }
+  end
+
   it 'should work' do
-    c1 = Course.create
-    c2 = Course.create
-    c1.replaces<< c2
+    c1 = Course.create!(@valid_course_attrs)
+    c2 = Course.create!(@valid_course_attrs)
+    c1.replaces << c2
     
     c1.replaces.must_include c2
     c1.replaced_with.wont_include c2
