35fbcbf12a24775ff38845035e19549f39d27d68
David Ford <dkford670@hotmail.com>
2012-01-10 18:45:03 -0800

    Add replacement information to course view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,17 +1,22 @@
 class Course < ActiveRecord::Base
   has_many :offerings
 
- #will show the fields
- def course_field
-   "#{dept_code} #{course_num} #{title}"
- end
-    
-    
- has_many :replacements_from, class_name: 'CourseReplacement', foreign_key: 'replace_id'
- has_many :replacements_to, class_name: 'CourseReplacement', foreign_key: 'with_id'
-    
- has_many :replaces, through: :replacements_to
- has_many :replaced_with, through: :replacements_from
-    
+  #ADD COMMENT
+  has_many :replacements_from, class_name: 'CourseReplacement', foreign_key: 'replace_id'
+  has_many :replacements_to, class_name: 'CourseReplacement', foreign_key: 'with_id'
 
+  #ADD COMMENT
+  has_many :replaces, through: :replacements_to
+  has_many :replaced_with, through: :replacements_from
+  
+  #will show the fields
+  def to_s
+    "#{dept_code}#{course_num}: #{title}"
+  end
+
+  def course_field
+    to_s
+  end
+
+ 
 end
