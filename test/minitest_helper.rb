b72d11346aee042845ae223b35f87cb91643f91f
Ryan Souza <ryan@ryansouza.net>
2012-01-16 15:47:44 -0800

    Update minitest-rails to more active repo
    Remove Test::Unit helper

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,18 +1,37 @@
 require "minitest/autorun"
 require "minitest/rails"
 
+# Uncomment if you want awesome colorful output
+# require "minitest/pride"
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 
 class MiniTest::Rails::Spec
+  # Uncomment if you want to support fixtures for all specs
+  # or
+  # place within spec class you want to support fixtures for
+  # include MiniTest::Rails::Fixtures
 
-  # Add methods to be used by all specs here...
-
+  # Add methods to be used by all specs here
 end
 
-# Uncomment to support fixtures in Model tests...
-# require "active_record/fixtures"
 class MiniTest::Rails::Model
-  # include ActiveRecord::TestFixtures
-  # self.fixture_path = File.join(Rails.root, "test", "fixtures")
+  # Add methods to be used by model specs here
+end
+
+class MiniTest::Rails::Controller
+  # Add methods to be used by controller specs here
+end
+
+class MiniTest::Rails::Helper
+  # Add methods to be used by helper specs here
+end
+
+class MiniTest::Rails::Mailer
+  # Add methods to be used by mailer specs here
+end
+
+class MiniTest::Rails::Integration
+  # Add methods to be used by integration specs here
 end
