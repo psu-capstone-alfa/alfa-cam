8a0c5752ff63b1156de9921f6ed6e8d4bceb5e7d
Ryan Souza <ryan@ryansouza.net>
2012-01-21 23:57:03 -0800

    Remove restrictions against editing

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,11 +2,10 @@ class Term < ActiveRecord::Base
   has_many :offerings
 
   before_destroy :check_for_offerings
-  before_update :check_for_offerings
 
   def check_for_offerings()
-    if self.offerings.count > 0
-      self.errors[:base] << "Sorry, you cannot edit or delete a term with offerings."
+    unless offering_ids.empty?
+      self.errors[:base] << "Sorry, you cannot delete a term with offerings."
       false
     end
   end
