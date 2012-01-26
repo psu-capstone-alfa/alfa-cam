d750ddf02973429c9b5d73d72d0566cc9ad3426a
Ryan Souza <ryan@ryansouza.net>
2012-01-26 15:25:26 -0800

    Migrate database schema again

--- db/schema.rb
+++ db/schema.rb
@@ -52,12 +52,6 @@ ActiveRecord::Schema.define(:version => 20120125091322) do
     t.datetime "updated_at"
   end
 
-  create_table "terms", :force => true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", :force => true do |t|
     t.string   "login"
     t.string   "name"
