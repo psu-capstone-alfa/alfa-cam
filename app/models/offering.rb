7cfb024d10cc3e707b70d654c62866bccf26474f
Eric O'Connell <eric@zerominuszero.net>
2012-01-29 22:13:39 -0800

    fix offering post AcademicTerm and add quick Course seeds

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -1,7 +1,7 @@
 #An offering is an instance of a course taught in a specific term
 class Offering < ActiveRecord::Base
   belongs_to :course
-  belongs_to :term
+  belongs_to :term, class_name: 'AcademicTerm', foreign_key: 'term_id'
 
   validates :course_id, :term_id, :presence => true
 end
