15ea9ddea51e74f553f614b0aa1f9332ef8d9a72
Ryan Souza <ryan@ryansouza.net>
2012-02-04 15:49:29 -0800

    Revert Offering.term_id migration
    Add some offering seed data

--- db/migrate/20120202165020_rename_term_id_to_academic_term_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameTermIdToAcademicTermId < ActiveRecord::Migration
-  def change 
-    rename_column :offerings, :term_id, :academic_term_id    
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create!( [
+users = User.create!( [
   { :login => 'review', :name => 'Acred Reviewer', :roles => [:reviewer] },
   { :login => 'inst1', :name => 'Instructor 1' , :roles => [:instructor]},
   { :login => 'inst2', :name => 'Instructor Staff' , :roles => [:instructor, :staff]},
@@ -15,7 +15,7 @@ User.create!( [
   { :login => 'admin', :name => 'Admin', :roles => [:admin]},
 ])
 
-Outcome.create!( [
+outcomes = Outcome.create!( [
   { key: 'A', title: "Outcome A", description: "I'm outcome A!!!" },
   { key: 'B', title: 'Outcome B', description: "I'm a B outcome..." },
   { key: 'C', title: 'C outcome with a longer title', description: "I have a long title to see how that works" },
@@ -33,5 +33,11 @@ courses = Course.create!( [
   {dept_code: 'CEE', course_num: '101', title: 'Engineering for Dummies'},
   {dept_code: 'CEE', course_num: '161', title: 'Intro to Civil Engineering'},
   {dept_code: 'CEE', course_num: '201', title: 'Cull the Flock'}
+])
+
+offerings = Offering.create!( [
+  { course: courses[0], term: terms[0], instructors: [users[1]] },
+  { course: courses[1], term: terms[0], instructors: [users[1]] },
+  { course: courses[2], term: terms[1], instructors: [users[2],users[1]] },
 ])
 
