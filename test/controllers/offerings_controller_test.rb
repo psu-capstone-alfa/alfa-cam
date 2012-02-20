b0ed7c73c827e1c60eca90c2d264fd39fca4cf69
Eric O'Connell <eric@zerominuszero.net>
2012-02-20 13:02:48 -0800

    Code cleanup
    Add teaches? and taught_by? question methods
    Refactor csv export
    Cleaned offering tests

--- test/controllers/offerings_controller_test.rb
+++ test/controllers/offerings_controller_test.rb
@@ -7,48 +7,47 @@ describe OfferingsController do
     get :index
     must_respond_with :success
   end
-  
+
   it "must export offerings" do
     @request.env["HTTP_ACCEPT"] = "*/*"
-    get :export
+    get :export, format: 'csv'
     must_respond_with :success
   end
-  
 
-=begin
   it "must get new" do
     get :new
     assert_response :success
   end
 
   it "must create offering" do
+    attributes = Factory(:offering).attributes
+    attributes.delete(:id)
     assert_difference('Offering.count') do
-      post :create, offering: {} 
+      post :create, offering: attributes
     end
     assert_redirected_to offering_path(assigns(:offering))
   end
 
   describe 'An existing offering' do
-  
+
     before do
-      @of = Offering.create!()
+      @offering = Factory(:offering)
     end
-    
+
     it "must show offering" do
-      get :show, id: @of.id
+      get :show, id: @offering.id
       assert_response :success
     end
 
     it "must get edit" do
-      get :edit, id: @of.id
+      get :edit, id: @offering.id
       assert_response :success
     end
 
     it "must update offering" do
-      put :update, id: @of.id, offering: @of.attributes
+      put :update, id: @offering.id, offering: @offering.attributes
       assert_redirected_to offering_path(assigns(:offering))
     end
-    
+
   end
-=end
 end
