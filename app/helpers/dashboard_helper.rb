4ad72ed212ef46e850beee8ec1a7c852d81e3cbc
Hoang Le <lcmhoang@gmail.com>
2012-02-19 18:44:22 -0800

    Dashboards status color now dynamic
    Update instructor and staff dashboards

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,13 @@
 module DashboardHelper
+  # Takes an offering and a stage(like :review, :content, ...)
+  # Returns a css class color ('green','yellow', 'red')
+  def stage_color(offering, stage)
+    if offering.complete_stages.include?(stage)
+      "green"
+    elsif offering.started_stages.include?(stage)
+      "yellow"
+    else
+      "red"
+    end
+  end
 end
