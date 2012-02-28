f425501be34d4d6879880fe9c99e8f5709f757ee
Charles <seadaily@gmail.com>
2012-02-27 17:04:16 -0800

    Spruce up login page
    Logout redirects to login page

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -42,6 +42,6 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = 'Logout successful'
-    redirect_back_or_default login_path
+    redirect_to login_path
   end
 end
