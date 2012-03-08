6014bd7ec5d12e9494365c300d4a36acad4f2fae
David Ford <dkford670@hotmail.com>
2012-03-07 19:33:39 -0800

    Course status is now dynamic

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,11 @@
 module CoursesHelper
+	def course_status_label(course)
+		output = ''.html_safe
+		if course.created_term_id != @term.id
+			output << content_tag(:span, 'Retired', class: 'label label-important')
+		else
+			output << content_tag(:span, 'Created', class: 'label label-info')
+		end
+		output
+	end
 end
---
+++ test/helpers/course_helper.test.rb
@@ -0,0 +1,5 @@
+require "minitest_helper"
+
+describe CourseHelper do
+
+end
